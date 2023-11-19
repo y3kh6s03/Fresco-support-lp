8dbe1655d4a25575c88976949f61e9634afedb08
yosuke026 <y3kh6s03@gmail.com>
2023-11-19 20:23:02 +0900

    redesign

--- app/components/service/Service.jsx
+++ app/components/service/Service.jsx
@@ -2,17 +2,26 @@ import Image from "next/image"
 
 import styles from "app/styles/components/service.module.scss"
 import items from "app/components/service/Service.json"
+import { useContext } from "react"
+import { FontsContext } from "@/app/page"
 
 export const Service = () => {
+    const [, Num] = useContext(FontsContext);
+
     return (
         <section className={styles.container}>
             {items.map((item, index) => {
                 return (
                     <article key={index} className={styles.service}>
                         <div className={styles.inner}>
-                            <span className={styles.inner__span}>
-                                service {item.num}
-                            </span>
+                            <div className={styles.inner__num}>
+                                <span className={styles.inner__num__span}>
+                                    service
+                                </span>
+                                <span className={`${Num.className} ${styles.inner__num__span}`}>
+                                    {item.num}
+                                </span>
+                            </div>
                             <h3 className={styles.inner__h3}>
                                 {item.headline}
                             </h3>
@@ -28,7 +37,7 @@ export const Service = () => {
                 )
             })}
             <div className={styles.svg}>
-                <Image src={"/svgs/arrow.svg"} fill sizes="100%" alt="bottom-arrow"/>
+                <Image src={"/svgs/arrow.svg"} fill sizes="100%" alt="bottom-arrow" />
             </div>
         </section>
     )
